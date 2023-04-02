d71c79eaad1ddd286a18ea20bc90e2c85e6803e0
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-02 23:12:23 +0700

    added pettype service. closes #15

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypService extends CrudService<PetType, Long> {
+}
