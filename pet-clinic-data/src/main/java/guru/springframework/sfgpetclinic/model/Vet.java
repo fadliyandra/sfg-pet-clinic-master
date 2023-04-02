79917e7e4b3857997db0eb619e2ffaded752a269
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-02 15:28:25 +0700

    added specialities to vet. closes #13

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -18,9 +18,13 @@ import java.util.Set;
 //@Table(name = "vets")
 public class Vet extends Person {
 
-//    @ManyToMany(fetch = FetchType.EAGER)
-//    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
-//            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
-//    private Set<Speciality> specialities = new HashSet<>();
+private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
