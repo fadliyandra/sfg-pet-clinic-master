fe14562ba75553c2a8b6a6b1402a43a8b66f40b4
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-03 11:42:03 +0700

    fixed link broken. closes #31

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -9,6 +9,10 @@ public class IndexController {
     @RequestMapping({"","/","index","index.html"})
     public String index(){
         return "index";
+    }
 
+    @RequestMapping("/oups")
+    public String upsHandler(){
+        return "notimplemented";
     }
 }
