00ae20a1732636f6400d0296018449d6c42dc28d
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-06 02:10:45 +0700

    Added spring jpa respositroy. closes #25

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/OwnerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.repository;
+
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+}
