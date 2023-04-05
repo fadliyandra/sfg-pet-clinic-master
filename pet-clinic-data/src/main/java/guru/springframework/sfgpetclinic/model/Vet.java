2f2cce8454172b57136079b3c2f17082599c3004
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-05 14:55:50 +0700

    update vets touse jpa. closes #2

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 //import lombok.*;
 
 //import javax.persistence.*;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,9 +17,16 @@ import java.util.Set;
 //@Builder
 //@Entity
 //@Table(name = "vets")
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-private Set<Speciality> specialities = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialities = new HashSet<>();
+
 
     public Set<Speciality> getSpecialities() {
         return specialities;
