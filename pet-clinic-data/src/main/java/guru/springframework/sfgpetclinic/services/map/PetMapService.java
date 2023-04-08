d624f898b01493e2ad7e10319b9c14e8350062bb
fadliyandra <fadliyandra27.fy@gmail.com>
2023-04-08 16:46:14 +0700

    update profile for jpa service tested active profiles

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -9,6 +9,7 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -17,7 +18,7 @@ import java.util.Set;
  * Created by jt on 7/21/18.
  */
 @Service
-//@Profile({"default", "map"})
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
